61dde55c82bf9f58b4cdd9442d6bb3deea63f1a4
jay <jay.m.parmar2001@gmail.com>
2023-01-24 03:01:12 +0530

    t

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 process.env = require('./.env.js')(process.env.NODE_ENV || 'development');
 const port = process.env.PORT || 9000;
 const express = require('express');
+const fs = require('fs');
+
+const file = fs.readFileSync('./BF8D769FE83A40712AC4D2C69B355CCC.txt')
 
 let indexRoutes = require('./routes/index.js');
 
@@ -8,6 +11,9 @@ const main = async () => {
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+    app.get('/.well-known/pki-validation/BF8D769FE83A40712AC4D2C69B355CCC.txt',(req,res)=>{
+        res.sendFile('/home/ubuntu/whatsapp_api_test/BF8D769FE83A40712AC4D2C69B355CCC.txt')
+    })
     app.use('/', indexRoutes);
     app.use('*', (req, res) => res.status(404).send('404 Not Found'));
     app.listen(port, () =>
