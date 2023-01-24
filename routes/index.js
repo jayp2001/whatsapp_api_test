e667ca863c200ead0866234d8a760b66e69172c8
vikalp chavda <vikalpchavda@vikalps-MacBook-Pro.local>
2023-01-24 21:51:17 +0530

    f

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 'use strict';
 const router = require('express').Router();
 
+const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
+const Whatsapp = new WhatsappCloudAPI({
+    accessToken: process.env.Meta_WA_accessToken,
+    senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
+    WABA_ID: process.env.Meta_WA_wabaId, 
+    graphAPIVersion: 'v15.0'
+});
+
 router.get('/hello',(req,res)=>{
     res.json("hello world !!!")
 })
@@ -23,19 +31,58 @@ router.get('/meta_wa_callbackurl', (req, res) => {
         } else {
             return res.sendStatus(403);
         }
+
     } catch (error) {
         console.error({error})
         return res.sendStatus(500);
     }
 });
 
+
+
 router.post('/meta_wa_callbackurl', async (req, res) => {
     try {
         console.log('POST: Someone is pinging me!');
+
+        let data = Whatsapp.parseMessage(req.body);
+
+        if (data?.isMessage) {
+            let incomingMessage = data.message;
+            let recipientPhone = incomingMessage.from.phone; // extract the phone number of sender
+            let recipientName = incomingMessage.from.name;
+            let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
+            let message_id = incomingMessage.message_id; // extract the message id
+
+            if (typeOfMsg === 'text_message') {
+                await Whatsapp.sendSimpleButtons({
+                    message: `Hey ${recipientName}, \nYou are speaking to a chatbot.\nWhat do you want to do next?`,
+                    recipientPhone: recipientPhone, 
+                    listOfButtons: [
+                        {
+                            title: 'Send PDF',
+                            id: 'send_pdf',
+                        },
+                    ],
+                });
+            }
+
+            if (typeOfMsg === 'simple_button_message') {
+                let button_id = incomingMessage.button_reply.id;
+            
+                if (button_id === 'send_pdf') {
+                    await Whatsapp.sendDocument({
+                        recipientPhone: "9825312229",
+                        file_path: `../document_recipt/recipte.pdf`,
+                    });
+                }
+            };
+    }
+
         return res.sendStatus(200);
     } catch (error) {
                 console.error({error})
         return res.sendStatus(500);
     }
 });
+
 module.exports = router;
