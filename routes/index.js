039a0d1ffe98af76e7095740a458bbb3b2d6caed
jay <jay.m.parmar2001@gmail.com>
2023-01-25 21:49:51 +0530

    g

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 'use strict';
 const router = require('express').Router();
 
-const WhatsappCloudAPI = require('../whatsappcloudapi_wrapper');
-const Whatsapp = new WhatsappCloudAPI({
-    accessToken: process.env.Meta_WA_accessToken,
-    senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
-    WABA_ID: process.env.Meta_WA_wabaId, 
-    graphAPIVersion: 'v15.0'
-});
+// const WhatsappCloudAPI = require('../whatsappcloudapi_wrapper');
+// const Whatsapp = new WhatsappCloudAPI({
+//     accessToken: process.env.Meta_WA_accessToken,
+//     senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
+//     WABA_ID: process.env.Meta_WA_wabaId, 
+//     graphAPIVersion: 'v15.0'
+// });
 
 router.get('/hello',(req,res)=>{
     res.json("hello world !!!")
